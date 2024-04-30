4900840289c5c919dd7d7791b718cc8994c3b33d
Leodan Valda <leodan.dev@gmail.com>
2024-04-30 14:55:04 -0400

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Bienvenidos a la inmersion Java");
+
+        int fechaLanzamiento = 1999;
+
+    }
+}
+
